a749ad263ac160ca05e4adf93c663fc067ab73c4
akashsingh7440 <akashsingh7440@gmail.com>
2025-08-01 18:15:36 +0530

    Episode-01 Learning

---
+++ App.js
@@ -0,0 +1,20 @@
+
+
+
+const heading = React.createElement("h1", 
+    {id: "heading", className: "headingclass"},
+    "Hello from React" );
+
+    const anotherHeading = React.createElement("h1", 
+    {id: "heading", className: "headingclass"},
+    "Hello from React 2.0" );
+const divChild = React.createElement("div", {id: "child"}, heading, anotherHeading);
+
+const divParent = React.createElement("div", {id: "parent"}, divChild);
+
+
+const pera = React.createElement("p", {}, "This is paragraph ");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(divParent);
