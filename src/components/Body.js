59c918159f0c544b58b032df41eafeecb8987fc8
akashsingh7440 <akashsingh7440@gmail.com>
2025-08-16 03:43:52 +0530

    Use the Live API call to Swiggy.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,33 @@
 import RestorentComponent from "./Restorent";
-import restaurantsData from "../utility/mockRestorentResponse";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const BodyComponent = () => {
-  const [resList, setResList] = useState(restaurantsData);
+  let restorentList;
+  const [resList, setResList] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.579343&lng=73.9089168&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const response = await data.json();
+    restorentList =
+      response.data.cards[1].card.card.gridElements.infoWithStyle.restaurants;
+    console.log(response);
+    setResList(restorentList);
+  };
+
   return (
     <div className="body">
       <div className="button-container">
         <button
           className="top-restaurants-btn"
           onClick={() => {
-            const topRestorent = restaurantsData.filter(
+            const topRestorent = resList.filter(
               (res) => res?.info?.avgRating > 4.3
             );
             setResList(topRestorent);
